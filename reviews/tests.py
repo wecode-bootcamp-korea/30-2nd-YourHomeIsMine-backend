cdce9d99a313fc622e65418e5c1ae26b75f5c6e6
kimeunji <78680486+thisiseunji@users.noreply.github.com>
2022-03-18 17:55:34 +0900

    [김은지] fix : review get

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,14 +1,103 @@
-# from django.test import TestCase, Client
+from django.test import TestCase, Client
 
-# from 
+from users.models   import User
+from rooms.models   import Category, Room
+from reviews.models import Review, ReviewImage
 
-# class ReviewTest(TestCase):
-#     def setUp(self):
-#         return super().setUp()
-    
-#     def test_get(self):
-#         self.assertEqual(,)
+class ReviewTest(TestCase):
+    def setUp(self):
+        test_user = User.objects.create(
+            id            = 1,
+            nickname      = "은지",
+            email         = "monahk93@google.com",
+            password      = "qqq",
+            profile_image = "1"
+        )
+        
+        test_category = Category.objects.create(
+            id   = 1,
+            type = "집 전체",
+        )
+        
+        test_room = Room.objects.create(
+            id                 = 1,
+            name               = "방1",
+            description        = "쓸쓸한방",
+            district           = "강남구",
+            neighberhood       = "역삼2동",
+            price              = 10000.00,
+            address            = "선릉로1길",
+            guests             = 5,
+            beds               = 5,
+            bedrooms           = 5,
+            baths              = 5,
+            check_in_time      = "13:00:00",
+            check_out_time     = "14:00:00",
+            is_instant_booking = True,
+            latitute           = 34.6750,
+            longitute          = 102.3043,
+            user               = test_user,
+            category           = test_category,
+        )
+        
+        test_review = Review.objects.create(
+            id               = 1,
+            content          = "내용",
+            accuracy         = 5,
+            communication    = 5,
+            cleanliness      = 5,
+            location         = 5,
+            check_in         = 5,
+            cost_performance = 5,
+            user             = test_user,
+            room             = test_room,
+        )
+        
+        ReviewImage.objects.create(
+            image_url ='1',
+            review    = test_review
+        )
+        
+    def tearDown(self):
+        User.objects.all().delete()
+        Category.objects.all().delete()
+        Room.objects.all().delete()
+        Review.objects.all().delete()
+        
+    def test_success_review_view_get_method(self):
+        client   = Client()
+        response = client.get("/rooms/1/reviews")
 
+        self.assertEqual(response.status_code, 200)
+        
+        self.assertEqual(response.json(), {
+            "message" : "SUCCESS", "result" : {
+                "reviews_number"  : 1,
+                "star_rating"     : 5.0,
+                "accuracy"        : 5.0,
+                "communication"   : 5.0,
+                "cleanliness"     : 5.0,
+                "location"        : 5.0,
+                "check_in"        : 5.0,
+                "cost_performance": 5.0,
+                "reviews"         : [
+                    {
+                        'profile_image' : '1',
+                        'contents'      : "내용",
+                        #'date'          : ,
+                        'review_image'  : ['1']
+                    }
+                ]
+            }
+        })
 
-# if __name__ == '__main__':
-#     unittest.main()
+    def test_review_view_get_room_not_exist(self):
+        client   = Client()
+        response = client.get("/rooms/2/reviews")
+
+        self.assertEqual(response.status_code, 400)
+        
+        self.assertEqual(response.json(), {
+            "message" : "ROOM_NOT_EXIST"
+            }
+        )
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,5 @@
 import boto3, uuid
+from datetime import datetime
 
 from django.http      import JsonResponse
 from django.views     import View
@@ -8,35 +9,56 @@ from django.db.models import Avg
 from users.models import User
 from rooms.models import Room
 from .models      import Review, ReviewImage
-from my_settings  import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+from my_settings  import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_S3_URL
 
 class ReviewView(View):   
     def get(self, request, room_id):
-        try:  
-            reviews = Room.objects.get(id=room_id).reviews.all()
-            average = reviews.aggregate(Avg('accuracy'), Avg('communication'), Avg('cleanliness'), Avg('location'), Avg('check_in'), Avg('cost_performance'))
-            result  = {
-                'reviews_number'  : reviews.count(),
+        try:
+            
+            limit   = int(request.GET.get('limit', 6))
+            page    = int(request.GET.get('page', 1))
+            offset  = (page-1) * limit           
+            reviews = Room.objects.get(id=room_id).reviews.all()[offset:offset+limit]
+            average = reviews.aggregate(
+                Avg('accuracy'), 
+                Avg('communication'),
+                Avg('cleanliness'), 
+                Avg('location'), 
+                Avg('check_in'), 
+                Avg('cost_performance')
+            )
+            
+            result = {
+                'reviews_number'  : Room.objects.get(id=room_id).reviews.all().count(),
                 'star_rating'     : round(sum(average.values())/len(average), 2),
                 'accuracy'        : round(average['accuracy__avg'], 2),
                 'communication'   : round(average['communication__avg'], 2),
                 'cleanliness'     : round(average['cleanliness__avg'], 2),
                 'location'        : round(average['location__avg'], 2),
-                'check_in'        : round(average['check_in__avg'], 2),
+                'check_in_rating' : round(average['check_in__avg'], 2),
                 'cost_performance': round(average['cost_performance__avg'], 2),
                 'reviews'         : [
                     {
+                        'review_id'     : review.id,
+                        'nickname'      : review.user.nickname,
                         'profile_image' : review.user.profile_image,
                         'contents'      : review.content,
-                        'date'          : review.created_at,
-                        'review_image'  : [review_image.image_url for review_image in review.review_images.all()]
+                        'date'          : datetime.strftime(review.created_at, '%y년 %m월').replace(' 0',' '),
+                        'review_image'  : [AWS_S3_URL + review_image.image_url for review_image in review.review_images.all()]
                     } for review in reviews]
             }
+            
             return JsonResponse({'message' : 'SUCCESS', 'result' : result}, status=200)
 
         except Room.DoesNotExist:
             return JsonResponse({'message' : 'ROOM_NOT_EXIST'}, status=400)
-             
+        
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
+        
+        except ValueError:
+            return JsonResponse({'message' : 'VALUE_ERROR'}, status=400)
+    
     # 로그인 데코레이터 같은 것 필요(user정보를 담고 있는)
     def post(self, request, room_id):
         try:
