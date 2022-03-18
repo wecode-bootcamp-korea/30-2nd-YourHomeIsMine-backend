016e93bc6fc51bed18692a525d8c28c8617c92fb
kkookkss <pre0919@gmail.com>
2022-03-18 14:37:16 +0900

    add: list api

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from rooms.views   import RoomDetailView
-from reviews.views import ReviewView 
+from rooms.views   import RoomDetailView,RoomListView
+from reviews.views import ReviewView
 
 urlpatterns = [
     path('/<int:room_id>', RoomDetailView.as_view()),
     path('/<int:room_id>/reviews', ReviewView.as_view()),
-    path('/<int:room_id>/reviews/<int:review_id>', ReviewView.as_view())
+    path('/<int:room_id>/reviews/<int:review_id>', ReviewView.as_view()),
+    path("", RoomListView.as_view())
 ]
